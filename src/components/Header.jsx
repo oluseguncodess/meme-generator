e19a6336db73872dde3f7f30adf0e76221064e3d
olusegun <olusegunbamgbelu755@gmail.com>
2024-09-30 10:03:37 +0100

    add modifications

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ function Header() {
         <header className="header">
             <img src="images/meme.jpeg" alt="meme" width={35} height={35} className="header--logo"/>
             <h2 className="header--title">Meme Generator</h2>
+            <img src="images/kratos-image.jpeg" alt="kratos logo" width={20} height={20} className="header--image_round"/>
             <h4 className="header--project">Kratos 战争</h4>
         </header>
     );
